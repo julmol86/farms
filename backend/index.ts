1d5463a8f57fdf4b06567bf8355c3047fce3e390
Yulia Mozhaeva <julia.mozhaeva@gmail.com>
2022-01-10 13:31:44 +0200

    added GET request for querying data

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,5 @@
 import { parseCsv } from './csvParserToDb'
-
+import sql from './db'
 const Koa = require('koa')
 const Router = require('@koa/router')
 const Cors = require('@koa/cors')
@@ -13,6 +13,42 @@ const router = new Router()
 
 router.get('/', (ctx: any) => {
   ctx.body = 'Hello World'
+})
+
+router.get('/data', async (ctx: any) => {
+  const params = ctx.request.query
+  console.log(params)
+  let queryBase = `
+    select
+      f.farmname,
+      fd.datetimestamp,
+      fd.metrictype,
+      fd.metricvalue
+    from farmdata fd
+    left join farm f on f.id = fd.farm_id
+  `
+
+  if (Object.keys(params).length !== 0) {
+    queryBase += ' where'
+    if (params.farm) {
+      queryBase += ` lower(f.farmname) like '${'%' + params.farm + '%'}'`
+    }
+    if (params.metrictype) {
+      queryBase += params.farm ? ' and' : ''
+      queryBase += ` fd.metrictype = '${params.metrictype}'`
+    }
+    if (params.startdate) {
+      queryBase += params.farm || params.metrictype ? ' and' : ''
+      queryBase += ` fd.datetimestamp >= '${params.startdate}'::date`
+    }
+    if (params.enddate) {
+      queryBase += params.farm || params.metrictype || params.startdate ? ' and' : ''
+      queryBase += ` fd.datetimestamp <= '${params.enddate}'::date`
+    }
+  }
+
+  ctx.status = 200
+  ctx.body = await sql.unsafe(queryBase + ' order by fd.datetimestamp')
 })
 
 app
