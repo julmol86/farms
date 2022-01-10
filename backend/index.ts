cf868224e4a4518d63342c087a13e3613e58c6e1
Yulia Mozhaeva <julia.mozhaeva@gmail.com>
2022-01-10 16:29:59 +0200

    added parameter validation

--- backend/index.ts
+++ backend/index.ts
@@ -10,6 +10,8 @@ parseCsv()
 
 const app = new Koa()
 const router = new Router()
+const ALLOWED_METRICS = ['temperature', 'rainfall', 'ph']
+const DATE_REGEX = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
 
 router.get('/', (ctx: any) => {
   ctx.body = 'Hello World'
@@ -27,28 +29,37 @@ router.get('/data', async (ctx: any) => {
     from farmdata fd
     left join farm f on f.id = fd.farm_id
   `
+  // param validation
+  /* add farm validation later */
+  const metricNotOk = params.metrictype && !ALLOWED_METRICS.includes(params.metrictype)
+  const startdateNotOk = params.startdate && (!DATE_REGEX.test(params.startdate) || (params.enddate && new Date(params.startdate) > new Date(params.enddate)))
+  const enddateNotOk = params.enddate && (!DATE_REGEX.test(params.enddate) || (params.startdate && new Date(params.startdate) > new Date(params.enddate)))
+  if (metricNotOk || startdateNotOk || enddateNotOk) {
+    ctx.status = 500
+    ctx.body = 'wrong param(s):' + (metricNotOk ? ' metrictype' : '') + (startdateNotOk ? ' startdate' : '') + (enddateNotOk ? ' enddate' : '')
+  } else {
+    if (Object.keys(params).length !== 0) {
+      queryBase += ' where'
+      if (params.farm) {
+        queryBase += ` lower(f.farmname) like '${'%' + params.farm + '%'}'`
+      }
+      if (params.metrictype) {
+        queryBase += params.farm ? ' and' : ''
+        queryBase += ` fd.metrictype = '${params.metrictype}'`
+      }
+      if (params.startdate) {
+        queryBase += params.farm || params.metrictype ? ' and' : ''
+        queryBase += ` fd.datetimestamp >= '${params.startdate}'::date`
+      }
+      if (params.enddate) {
+        queryBase += params.farm || params.metrictype || params.startdate ? ' and' : ''
+        queryBase += ` fd.datetimestamp <= '${params.enddate}'::date`
+      }
+    }
 
-  if (Object.keys(params).length !== 0) {
-    queryBase += ' where'
-    if (params.farm) {
-      queryBase += ` lower(f.farmname) like '${'%' + params.farm + '%'}'`
-    }
-    if (params.metrictype) {
-      queryBase += params.farm ? ' and' : ''
-      queryBase += ` fd.metrictype = '${params.metrictype}'`
-    }
-    if (params.startdate) {
-      queryBase += params.farm || params.metrictype ? ' and' : ''
-      queryBase += ` fd.datetimestamp >= '${params.startdate}'::date`
-    }
-    if (params.enddate) {
-      queryBase += params.farm || params.metrictype || params.startdate ? ' and' : ''
-      queryBase += ` fd.datetimestamp <= '${params.enddate}'::date`
-    }
+    ctx.status = 200
+    ctx.body = await sql.unsafe(queryBase + ' order by fd.datetimestamp')
   }
-
-  ctx.status = 200
-  ctx.body = await sql.unsafe(queryBase + ' order by fd.datetimestamp')
 })
 
 app
