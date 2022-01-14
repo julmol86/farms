ad8f22e9b132bf541532a62e9f20896d96cae2b6
Yulia Mozhaeva <julia.mozhaeva@gmail.com>
2022-01-14 22:45:47 +0200

    fixed month parameter check

--- backend/constants.ts
+++ backend/constants.ts
@@ -1,4 +1,4 @@
 export const CSV_FOLDER = './csv'
 export const ALLOWED_METRICS = ['temperature', 'rainfall', 'ph']
-export const ALLOWED_MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+export const ALLOWED_MONTHS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 export const DATE_REGEX = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
